b32fc00a0dd0cce8b8efc3b933fbb7ee427aad4a
Chris Ren <ren.chris@outlook.com>
2025-09-08 17:41:57 -0700

    style(eslintrc): remove rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,34 +17,8 @@ module.exports = {
   parserOptions: {
       project: "./tsconfig.json"
   },
-  "rules": {
-      "import/extensions": [
-          "error",
-          "ignorePackages",
-          {
-              "js": "never",
-              "jsx": "never",
-              "ts": "never",
-              "tsx": "never"
-          }
-      ],
-      "react/function-component-definition": [1, {
-              "namedComponents": "arrow-function"
-      }],
-      "react/jsx-props-no-spreading": [1, {
-          "html": "ignore",
-      }],
-      "no-use-before-define": "off",
-      "arrow-body-style": ["error", "as-needed"],
-      "@typescript-eslint/no-use-before-define": ["error"],
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/semi": ["error", "never"],
-      "react/jsx-filename-extension": [
-          1,
-          { "extensions": [".js", ".jsx", ".ts", ".tsx"] }
-      ]
-  },
-  "settings": {
+  rules: {},
+  settings: {
       "import/resolver": {
           "node": {
               "extensions": [".js", ".jsx", ".ts", ".tsx"]
